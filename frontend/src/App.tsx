018561a717ce881319cce32811bc7b9e8f0c7d9d
Jackson Brough <jacksontbrough@gmail.com>
2025-09-16 17:25:10 -0600

    Let's try that again

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button>
+          count is 4
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn less
+      </p>
+    </>
+  )
+}
