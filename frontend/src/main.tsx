e2e4999d29ff3eb947dcfaa89ead14814029d555
Jackson Brough <jacksontbrough@gmail.com>
2025-12-06 13:41:17 -0700

    Italic fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/ibm-plex-serif";
+import "@fontsource/ibm-plex-serif/400.css";
+import "@fontsource/ibm-plex-serif/600.css";
+import "@fontsource/ibm-plex-serif/700.css";
+import "@fontsource/ibm-plex-serif/400-italic.css";
+import "@fontsource/ibm-plex-serif/600-italic.css";
+import "@fontsource/ibm-plex-serif/700-italic.css";
 
 import App from "./App.tsx";
 import "./index.css";
